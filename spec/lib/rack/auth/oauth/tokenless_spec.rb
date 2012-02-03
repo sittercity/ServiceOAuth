05c096183bd9399f689aeced1c60953db680c0aa
Matt Jones + Tony Schneider <tonywok+mj+ts@gmail.com>
2012-02-02 19:55:35 -0600

    clean up specs

--- spec/lib/rack/auth/oauth/tokenless_spec.rb
+++ spec/lib/rack/auth/oauth/tokenless_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe Rack::Auth::Oauth::Tokenless do
-  def dummy_app; lambda{|env| [200, {}, [ env["oauth_client"] ]]}; end
+
+  def dummy_app; lambda{|env| [200, {}, []]}; end
 
   let(:middleware) { Rack::Auth::Oauth::Tokenless.new(dummy_app) }
   let(:mock_request) { Rack::MockRequest.new(middleware) }
@@ -9,11 +10,13 @@ describe Rack::Auth::Oauth::Tokenless do
   context 'request has no auth header' do
 
     it 'returns a 401' do
-      mock_request.get("/").status.should == 401
+      resp = mock_request.get("/")
+      resp.status.should == 401
     end
 
     it 'notifies client that they are unauthorized' do
-      mock_request.get("/").body.should == "Unauthorized"
+      resp = mock_request.get("/")
+      resp.body.should == "Unauthorized"
     end
 
   end
@@ -21,99 +24,93 @@ describe Rack::Auth::Oauth::Tokenless do
   context 'request has an authorization header' do
     context 'has an incorrect Authorization scheme' do
 
+      let(:bad_header) { "JUNK" }
+
       it 'returns a 401' do
-        mock_request.get("/", "HTTP_AUTHORIZATION" => "JUNK").status.should == 401
+        resp = mock_request.get("/", "HTTP_AUTHORIZATION" => bad_header)
+        resp.status.should == 401
       end
 
       it 'notifies client that they are unauthorized' do
-        mock_request.get("/", "HTTP_AUTHORIZATION" => "JUNK").body.should == "Incorrect authorization scheme, must be OAuth 1.0"
+        resp = mock_request.get("/", "HTTP_AUTHORIZATION" => bad_header)
+        resp.body.should == "Incorrect authorization scheme, must be OAuth 1.0"
       end
 
     end
 
     context 'does not have an oauth_consumer_key' do
 
+      let(:header_without_consumer_key) {
+        { "HTTP_AUTHORIZATION" => "OAuth realm=\"foo\"" }
+      }
+
       it 'returns a 401' do
-        mock_request.get("/", "HTTP_AUTHORIZATION" => "OAuth realm=\"foo\"").status.should == 401
+        resp = mock_request.get("/", header_without_consumer_key)
+        resp.status.should == 401
       end
 
       it 'notifies the client that they provided not included their consumer key' do
-        mock_request.get("/", "HTTP_AUTHORIZATION" => "OAuth realm=\"foo\"").body.should == "Unspecified OAuth consumer key"
+        resp = mock_request.get("/", header_without_consumer_key)
+        resp.body.should == "Unspecified OAuth consumer key"
       end
 
     end
 
     context 'does not have an oauth_signature' do
 
+      let(:header_without_signature) {
+        { "HTTP_AUTHORIZATION" => "OAuth realm=\"foo\", oauth_consumer_key=\"123\"" }
+      }
+
       it 'returns a 401' do
-        mock_request.get("/", "HTTP_AUTHORIZATION" => "OAuth realm=\"foo\", oauth_consumer_key=\"123\"").status.should == 401
+        resp = mock_request.get("/", header_without_signature)
+        resp.status.should == 401
       end
 
       it 'notifies the client that they provided not included their consumer key' do
-        mock_request.get("/", "HTTP_AUTHORIZATION" => "OAuth realm=\"foo\", oauth_consumer_key=\"123\"").body.should == "Missing signature in OAuth request"
+        resp = mock_request.get("/", header_without_signature)
+        resp.body.should == "Missing signature in OAuth request"
       end
 
     end
 
     context 'does not have an oauth_signature_method' do
 
+      let(:header_without_sig_method) {
+        { "HTTP_AUTHORIZATION" => "OAuth realm=\"foo\", oauth_consumer_key=\"123\", oauth_signature=\"SIGNATURE\"" }
+      }
+
       it 'returns a 401' do
-        auth_header = "OAuth realm=\"foo\", oauth_consumer_key=\"123\", oauth_signature=\"SIGNATURE\""
-        mock_request.get("/", "HTTP_AUTHORIZATION" => auth_header).status.should == 401
+        resp = mock_request.get("/", header_without_sig_method)
+        resp.status.should == 401
       end
 
       it 'notifies the client that they provided not included their consumer key' do
-        auth_header = "OAuth realm=\"foo\", oauth_consumer_key=\"123\", oauth_signature=\"SIGNATURE\""
-        mock_request.get("/", "HTTP_AUTHORIZATION" => auth_header).body.should == "Missing signature method in OAuth request"
+        resp = mock_request.get("/", header_without_sig_method)
+        resp.body.should == "Missing signature method in OAuth request"
       end
 
     end
 
     context 'has oauth_consumer_key, oauth_signature, and oauth_signature_method and specifies OAuth protocol' do
 
-      let!(:env) { Rack::MockRequest.env_for('/') }
-      let!(:request) { Rack::Request.new(env) }
-      let!(:consumer_key) { DummyClient::DUMMY_KEY }
-      let!(:consumer_secret) { DummyClient::DUMMY_SECRET }
-      let!(:nonce) { OAuth::Helper.generate_nonce }
-      let!(:timestamp) { OAuth::Helper.generate_timestamp }
-      let!(:signature_method) { "HMAC-SHA1" }
-      # let!(:signature) {
-      #   CGI.escape(OAuth::Signature.sign(request, :parameters => {
-      #     'oauth_consumer_key' => consumer_key,
-      #     'oauth_consumer_secret' => consumer_secret,
-      #     'oauth_signature_method' => signature_method,
-      #     'oauth_nonce' => nonce,
-      #     'oauth_timestamp' => timestamp,
-      #     'oauth_version' => '1.0'
-      #   }))
-      # }
-      let!(:consumer) {
-        OAuth::Consumer.new(consumer_key, consumer_secret, {})
-      }
-      let!(:client_helper) {
-        OAuth::Client::Helper.new(request, { :consumer => consumer })
-      }
-      let!(:auth_header) { client_helper.header }
-      # let!(:bad_auth_header) {
-      #   ["OAuth realm=\"foo\"",
-      #    "oauth_consumer_key=\"#{consumer_key}\"",
-      #    "oauth_signature=\"#{signature}\"",
-      #    "oauth_nonce=\"#{OAuth::Helper.generate_nonce}\"",
-      #    "oauth_timestamp=\"#{OAuth::Helper.generate_timestamp}\"",
-      #    "oauth_version=\"1.0\"",
-      #    "oauth_signature_method=\"#{signature_method}\""].join(',')
-      # }
+      let(:env) { Rack::MockRequest.env_for('/') }
+      let(:request) { Rack::Request.new(env) }
+      let(:consumer_key) { DummyClient::DUMMY_KEY }
+      let(:consumer_secret) { DummyClient::DUMMY_SECRET }
+      let(:consumer) { OAuth::Consumer.new(consumer_key, consumer_secret) }
+      let(:client_helper) { OAuth::Client::Helper.new(request, { :consumer => consumer }) }
+      let(:valid_auth_header) { { "HTTP_AUTHORIZATION" => client_helper.header } }
 
       before do
         Rack::Auth::Oauth::Tokenless.stub(:client_class => 'DummyClient')
       end
 
       it 'has a successful response' do
-        resp = mock_request.get("/", "HTTP_AUTHORIZATION" => auth_header)
-
+        resp = mock_request.get("/", valid_auth_header)
         resp.status.should == 200
       end
+
     end
   end
 end
